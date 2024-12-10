9798a970bea225ab38ba06dfeee58c8f5c44e092
n0thhhing <137664746+n0thhhing@users.noreply.github.com>
2024-12-10 10:53:13 -0500

    add build

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const mod = b.addModule("zig-neon", .{
+        .root_source_file = b.path("src/zig-neon.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = mod.root_source_file.?,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
